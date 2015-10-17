48b42d8a17c38df7eb868e609f3f0478945d8734
Ananth Rao <ananthamapod@gmail.com>
2015-10-16 22:29:55 -0400

    Slight optimization for loop in algorithm. Add Comments

--- NCombinatoR.js
+++ NCombinatoR.js
@@ -1,38 +1,69 @@
-  function NCombinatoR(n, r) {
-    //inner function for heavy lifting
-    var comb = function(indices) {
-      if(indices === undefined) {
-        indices = [];
-      }
-      var ret = [];
-      if(indices.length < r-1) {
-        var i = 0;
-        if(indices.length !== 0) {
-          i = indices[indices.length-1] + 1;
-        }
-        for(i; i < n; i++) {
-          var arr = indices.slice(0);
-          arr.push(i);
-          var combs = comb(arr);
-          for(var j = 0; j < combs.length; j++) {
-            ret.push(combs[j]);
-          }
-        }
-      } else {
-        ret = [];
-        var test = indices[indices.length-1];
-        if(test < n-1) {
-          test++;
-          while(test <= n-1) {
-            var arr = indices.slice(0);
-            arr.push(test);
-            ret.push(arr);
-            test++;
-          }
-        }
-      }
-      return ret;
-    }
+/*
+ * Name: NCombinatoR
+ * Description: Computes indices for the set of combinations when given the size of the list of outcomes,
+   and the number of items of interest
+ * Arguments:
+      n - size of the list of outcomes
+      r - number of items of interest
+ * Return: array of combinations
+ */
 
-    return comb();
+function NCombinatoR(n, r) {
+  //check for incorrect arguments
+  if(!isInt(n) || !isInt(r)) {
+    return [];
   }
+
+  //utility to check if argument is an integer
+  var isInt = function(n) {
+    return parseInt(n) === n;
+  };
+
+  //inner function for heavy lifting
+  var comb = function(indices) {
+
+    if(indices === undefined) {
+      indices = [];
+    }
+    //return array of combinations
+    var ret = [];
+    //for all but the last index of the combination, recurse
+    if(indices.length < r-1) {
+      //set the iterator to start at 0 to account for the first index of the combination
+      var i = 0;
+      //if not in the first index of the combination, the iterator starts at the point beyond the previous index
+      if(indices.length !== 0) {
+        i = indices[indices.length-1] + 1;
+      }
+
+      for(i; i < n-(r-indices.length-1); i++) {
+        var arr = indices.slice(0);
+        arr.push(i);
+
+        //add all combinations generated to the return list
+        Array.prototype.push.apply(ret, comb(arr));
+      }
+    //at the last index of the combination, escape condition, don't recurse
+    } else {
+      var test = indices[indices.length-1];
+      if(test === undefined) {
+        test = -1;
+      }
+
+      while(test < n-1) {
+        test++;
+        var arr = indices.slice(0);
+        arr.push(test);
+
+        ret.push(arr);
+      }
+    }
+    return ret;
+  }
+
+  return comb([]);
+}
+
+var combs = NCombinatoR("",1);
+console.log(combs);
+console.log(combs.length);
